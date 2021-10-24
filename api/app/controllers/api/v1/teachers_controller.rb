6763ce1cb21cb0246bca848109138b417f66c849
Atsu77 <kamnknon@gmail.com>
2021-10-25 08:59:16 +0900

    teacherのコントローラーを作成

---
+++ api/app/controllers/api/v1/teachers_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::TeachersController < ApplicationController
+  before_action :require_user_logged_in, only: %i(update)
+
+  def show
+    @teacher = Teacher.find(params[:id])
+
+    render 'show.json.jb'
+  end
+
+  def create
+    @teacher = Teacher.new(new_teacher_params)
+
+    if @teacher.save
+      session[:user_id] = @teacher.id
+
+      render 'show.json.jb'
+    else
+      render_errors(@teacher)
+    end
+  end
+
+  def new_teacher_params
+    params.require(:teacher).permit(:name, :email, :password, :password_confirmation)
+  end
+end
