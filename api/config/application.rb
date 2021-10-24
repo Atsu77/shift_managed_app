bc24cf76a4a01019897696a6ba75d3e253dce7f7
Atsu77 <kamnknon@gmail.com>
2021-10-24 10:52:32 +0900

    rails日本語対応

--- api/config/application.rb
+++ api/config/application.rb
@@ -40,9 +40,10 @@ module Myapp
                        request_specs: false
     end
     #日本語ファイルの読み込み 
-    #config.time_zone = 'Tokyo'
-    #config.active_record.default_timezone = :local
-    #config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
 
     config.api_only = true
