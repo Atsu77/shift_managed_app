9c330a796fed61ac1bb2921f9c983da63e12118a
Atsu77 <kamnknon@gmail.com>
2021-10-25 17:44:58 +0900

    studentmodelの実装

---
+++ api/app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::StuentsController < ApplicationController
+  before_action :require_user_logged_in, only: %i(update)
+
+  def show
+    @student = Student.find(params[:id])
+
+    render 'show.json.jb'
+  end
+
+  def create
+    @student = student.new(new_student_params)
+
+    if @student.save
+      session[:user_id] = @student.id
+
+      render 'show.json.jb'
+    else
+      render_errors(@student)
+    end
+  end
+
+  def new_student_params
+    params.require(:student).permit(:name, :email, :password, :password_confirmation)
+  end
+end
