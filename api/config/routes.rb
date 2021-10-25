373f4d11ebba60fd7024cf89ca7c47fd89bd1e39
Atsu77 <kamnknon@gmail.com>
2021-10-25 09:14:51 +0900

    sessionのrouteを定義

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :teachers, only: %i[create update show] 
+      resources :teachers, only: %i(create update show)
+      resource :sessions, only: %i(show create destroy)
     end
   end
 end
