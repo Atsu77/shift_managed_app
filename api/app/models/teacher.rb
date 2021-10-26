12b8423426fa3dd35da7544b9d0e4a1d17760ad8
Atsu77 <kamnknon@gmail.com>
2021-10-26 10:00:51 +0900

    subjectとteacherとsubject_teacherのmodelファイルを作成

--- api/app/models/teacher.rb
+++ api/app/models/teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < ApplicationRecord
   has_secure_password
+  has_many :subject_teachers, through: :subjects
 
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
