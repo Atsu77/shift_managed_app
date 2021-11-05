feebab17354f84ea557fdb31c72992bf5dc245e7
Atsu77 <kamnknon@gmail.com>
2021-11-05 10:14:56 +0900

    教師の担当科目を登録用のapiエンドポイント設定

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       resources :teachers, only: %i(create update show) do
+        resources :subjects, only: %i(show create update destroy)
         resources :komas, only: %i(index create update destroy)
       end
+
       resources :students, only: %i(create update show)
       
       resource :sessions, only: %i(show create destroy) do
