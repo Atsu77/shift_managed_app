df13d236dd0775bdfd1e8816366766b88de97b1d
Atsu77 <kamnknon@gmail.com>
2021-11-18 13:57:39 +0900

    apiのurlエンドポイントを記載するファイルを作成

---
+++ front/app/src/urls/index.ts
@@ -0,0 +1,4 @@
+const DEFAULT_API_LOCALHOST = "http://localhost:3001/api/v1";
+
+export const sessions = `${DEFAULT_API_LOCALHOST}/sessions`
+export const TeacherIndex = `${DEFAULT_API_LOCALHOST}/teachers`
