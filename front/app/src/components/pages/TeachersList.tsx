3fd0ddeff51c4befcc9c24c501b5788a64189121
Atsu77 <kamnknon@gmail.com>
2021-11-20 15:48:04 +0900

    講師一覧を取得できることを確認

---
+++ front/app/src/components/pages/TeachersList.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Teacher } from "../../redux/teachers/types";
+import { TeacherIndex } from "../../urls";
+
+const TeachersList = () => {
+
+  const [teacherList, setTeacherList] = useState<Array<Teacher>>([])
+
+  useEffect(() => {
+    axios.get(TeacherIndex)
+    .then((res) => setTeacherList(res.data.teachers))
+    .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <div>
+      {teacherList.map((teacher) => (
+        <ul>
+          <li>{teacher.id}</li>
+          <li>{teacher.name}</li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default TeachersList;
