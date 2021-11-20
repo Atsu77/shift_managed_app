6484af38c8a9e1db6b6d94db6d22eb72aa0da745
Atsu77 <kamnknon@gmail.com>
2021-11-20 15:57:48 +0900

    reset.cssを導入

--- front/app/src/index.tsx
+++ front/app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as History from 'history';
-import './index.css';
+import './assets/reset.css'
 import App from './App';
 import createStore from './redux/store/store';
 import { Provider } from 'react-redux';
