86242acf1c6dd54344ff0eaec4d14fd1b814508b
Atsu77 <kamnknon@gmail.com>
2021-11-21 13:30:59 +0900

    tailwindcssを導入

--- front/app/src/index.tsx
+++ front/app/src/index.tsx
@@ -6,6 +6,9 @@ import './assets/reset.css'
 import App from './App';
 import createStore from './redux/store/store';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { theme } from './assets/theme';
+import './index.css'
 
 const history = History.createBrowserHistory();
 export const store = createStore(history);
@@ -13,7 +16,9 @@ export const store = createStore(history);
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
+      <MuiThemeProvider theme={theme}>
       <App />
+      </MuiThemeProvider>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
