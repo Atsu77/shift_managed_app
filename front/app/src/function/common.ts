e144acb557810bb7f08208625abb87a7603ede34
Atsu77 <kamnknon@gmail.com>
2021-11-21 09:33:13 +0900

    ユーザー認証のバリデーションの関数を追加

--- front/app/src/function/common.ts
+++ front/app/src/function/common.ts
@@ -1,15 +1,27 @@
 /**
- * @brief inputが空でないかの確認 
+ * @brief メールのフォーマットを確認f
+ * @param {string} email
+ * @returns {boolean}
+ */
+
+export const isValidEmailFormat = (email: string) => {
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return regex.test(email);
+};
+
+/**
+ * @brief inputが空でないかの確認
  * @param args inputの値
  * @returns {boolean}
  */
 
 export const isValidRequiredInput = (...args: any[]) => {
   let validator = true;
-  for (let i=0; i < args.length; i=(i+1)|0) {
-      if (args[i] === "") {
-          validator = false;
-      }
+  for (let i = 0; i < args.length; i = (i + 1) | 0) {
+    if (args[i] === "") {
+      validator = false;
+    }
   }
-  return validator
+  return validator;
 };
