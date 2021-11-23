062f55837c004cc286887a659880d69e73347a56
Atsu77 <kamnknon@gmail.com>
2021-11-23 10:19:52 +0900

    module-spacerを導入

--- front/app/src/App.tsx
+++ front/app/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import PrimaryButton from './components/atoms/PrimaryButton';
+import TextInput from './components/atoms/TextInput';
+import AuthForm from './components/molecules/AuthForm';
 import Router from './Router';
 
 function App() {
   return (
       <BrowserRouter>
+        <AuthForm label={"test"}/>
         <Router />
       </BrowserRouter>
   );
