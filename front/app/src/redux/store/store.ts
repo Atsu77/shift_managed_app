247d7a487fd5c794757cf2bbea99ff3662647ad9
Atsu77 <kamnknon@gmail.com>
2021-11-25 12:02:01 +0900

    redux-dev-toolを導入

--- front/app/src/redux/store/store.ts
+++ front/app/src/redux/store/store.ts
@@ -1,19 +1,28 @@
-import { routerMiddleware } from "connected-react-router";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import {
   applyMiddleware,
   combineReducers,
   createStore as reduxCreateStore,
+  compose
 } from "redux";
 import thunk from "redux-thunk";
-import { TeacherReducer } from "../teachers/reducers";
+import TeacherReducer from "../teachers/reducers";
+
+interface ExtendedWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+}
+
+declare var window: ExtendedWindow;
 
 export default function createStore(history: any) {
+  const composeReduxDevToolsEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   let middleWares = [routerMiddleware(history), thunk];
 
   return reduxCreateStore(
     combineReducers({
-      teachers: TeacherReducer,
+      teacher: TeacherReducer,
+      router: connectRouter(history),
     }),
-    applyMiddleware(...middleWares)
+    composeReduxDevToolsEnhancers(applyMiddleware(...middleWares))
   );
 }
