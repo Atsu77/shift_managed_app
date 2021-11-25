0d05e45b5a994b38e4a35c0c6acc9baa41e2dcd4
Atsu77 <kamnknon@gmail.com>
2021-11-25 19:48:37 +0900

    仮のHomeページを作成

--- front/app/src/Router.tsx
+++ front/app/src/Router.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import {Switch, Route} from 'react-router';
 import TeachersList from './components/pages/TeachersList';
 import SignUp from './components/pages/SignUp';
+import Home from './components/pages/Home';
+import Auth from './components/templates/Auth';
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path="/teachers" component={TeachersList}/>
       <Route exact path="/signup" component={SignUp} />
+      <Auth>
+        <Route exact path="(/)?" component={Home} />
+      </Auth>
     </Switch>
   )
 }
---
+++ front/app/src/components/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div style={{ color: " #fff", fontWeight: "bold" }}>Homeページ</div>;
+};
+
+export default Home;
